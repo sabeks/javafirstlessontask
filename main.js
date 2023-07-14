a733369852d68620c4fece80494458db78745ffc
saba aptsiauri <llamerchik@gmail.com>
2023-07-15 02:09:41 +0400

    added console log my information and primtive types

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log("hello world");
+
+let nameUser = "sabeks";
+let ageUser = 17;
+let isMale = true;
+let undefined1 = undefined;
+
+console.log(typeof nameUser, ageUser, isMale);
+let myName = "saba";
+let myHobby = "playning games";
+let yearOfBirth = 2006;
+let currentYear = 2023;
+
+console.log(typeof myName, myHobby, yearOfBirth, currentYear);
+console.log(
+  "My name is" +
+    myName +
+    "i am" +
+    currentYear -
+    yearOfBirth +
+    "old and my hobby is" +
+    myHobby
+);
